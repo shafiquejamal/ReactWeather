558d3a07965ca0ddc021f8aa651d14f307568495
Shafique Jamal <shafique.jamal@gmail.com>
2016-06-11 10:32:27 -0400

    Add styling

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
       Examples: 'app/components/Examples.jsx',
       About: 'app/components/About.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
-      ErrorModal: 'app/components/ErrorModal.jsx'
+      ErrorModal: 'app/components/ErrorModal.jsx',
+      applicationStyles: 'app/styles/app.css',
     },
     extensions: ['', '.js', '.jsx']
   },
