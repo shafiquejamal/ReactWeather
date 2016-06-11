733b9228cee3a23fa62013ed171219f60c6f480e
Shafique Jamal <shafique.jamal@gmail.com>
2016-06-11 10:45:16 -0400

    Switch to SCSS

--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
       About: 'app/components/About.jsx',
       openWeatherMap: 'app/api/openWeatherMap.jsx',
       ErrorModal: 'app/components/ErrorModal.jsx',
-      applicationStyles: 'app/styles/app.css',
+      applicationStyles: 'app/styles/app.scss',
     },
     extensions: ['', '.js', '.jsx']
   },
