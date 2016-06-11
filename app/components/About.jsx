17bda1136001f2381945e7efd1b01028b0fbde06
Shafique Jamal <shafique.jamal@gmail.com>
2016-06-11 00:25:35 -0400

    Style about page and link to resources

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,10 +1,22 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 var About = (props) => {
   return (
     <div>
-      <h3>About</h3>
+      <h1 className="text-center">About</h1>
       <p>Welcome to the about page.</p>
+      <ul>
+        <li>
+          <a href="https://facebook.github.io/react/">React</a> - The JavaScript Framework.
+        </li>
+        <li>
+          <a href="http://openweathermap.org/">Open Weather Map</a> - The map info source.
+        </li>
+        <li>
+          <a href="https://github.com/shafiquejamal/ReactWeather">Github repo</a> - The github repo.
+        </li>
+      </ul>
     </div>
   );
 };
